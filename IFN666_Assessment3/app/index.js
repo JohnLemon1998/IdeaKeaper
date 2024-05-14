a73e4e3189f43e979b4fe1f154672df5c62fd597
Komei Soda <65644039+JohnLemon1998@users.noreply.github.com>
2024-05-14 21:09:33 +1000

    Home page added
    Home page added

---
+++ IFN666_Assessment3/app/index.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image, StyleSheet, Dimensions } from 'react-native';
+
+const WelcomePage = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/images/homePageIcon.webp')}
+        style={styles.image}
+      />
+      <Text style={styles.title}>Welcome to Idea Keeper App</Text>
+      <Text style={styles.subtitle}>Your idea won't be missing anymore</Text>
+      
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Signup')}>
+          <Text style={styles.buttonText}>SignUp</Text>
+        </TouchableOpacity>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  image: {
+    width: '100%',
+    height: 400,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    margin: 50,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    marginTop: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 50,
+    width: '100%',
+  },
+  button: {
+    width: '45%',
+    height: 40,
+    backgroundColor: '#007AFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default WelcomePage;
